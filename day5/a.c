3e02282693789f1bbb414c4628d9011152e96c95
Jaldemark <joeljaldemark@outlook.com>
2021-12-10 19:41:31 +0100

    Update a.c

--- day5/a.c
+++ day5/a.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 #define ASCII_OFFSET 48
 #define SIZE 990
 
@@ -43,10 +44,8 @@ void main()
           }
         }
       }
-      for (int i = 0; i < 4; i++)
-      {
-        line_coordinates[i] = 0;
-      }
+      memset(line_coordinates, 0, sizeof(line_coordinates));
+
       number_index = 0;
     }
   }
